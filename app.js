99ebcc43edaf27d64b87dd9966c876cf8136a9e8
Joseph Nechleba <josephnechleba@Josephs-MacBook-Pro.local>
2022-04-02 15:47:14 -0400

    remove helmet

--- app.js
+++ app.js
@@ -1,17 +1,10 @@
 require("dotenv").config();
 require("./config/database").connect();
-var helmet = require('helmet');
 const express = require("express");
 
 const app = express();
 
 app.use(express.json());
-app.use(helmet.contentSecurityPolicy({
-    useDefaults: true,
-    directives: {
-      "img-src": ["'self'", "https: data:"]
-    }
-}));
 app.use(express.static(__dirname + '/dist/portfolio'));
 
 app.get('/pokemon', (req, res) => {
