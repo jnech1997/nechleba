e58242bafdb6c6388b1f9532454279048e60b42b
Joseph Nechleba <josephnechleba@Josephs-MacBook-Pro.local>
2022-04-02 16:26:18 -0400

    remove helmet, generalize api request handling

--- app.js
+++ app.js
@@ -1,20 +1,14 @@
 require("dotenv").config();
 require("./config/database").connect();
 const express = require("express");
-var helmet = require('helmet');
 var indexRouter = require('./routes/index');
 
 const app = express();
 
 app.use(express.json());
-app.use(
-    helmet({
-        contentSecurityPolicy: false,
-    })
-);
 app.use(express.static(__dirname + '/dist/portfolio'));
 
-app.use('/api', indexRouter);
+app.use('/api/:request', indexRouter);
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname)));
 
