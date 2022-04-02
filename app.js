4baf40a17b34f974170d6e0dece96578aedb2a65
Joseph Nechleba <josephnechleba@Josephs-MacBook-Pro.local>
2022-04-02 16:33:17 -0400

    debugging pokemon call

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 require("dotenv").config();
 require("./config/database").connect();
 const express = require("express");
-var indexRouter = require('./routes/index');
+var apiRouter = require('./routes/api');
 
 const app = express();
 
 app.use(express.json());
 app.use(express.static(__dirname + '/dist/portfolio'));
 
-app.use('/api/:request', indexRouter);
+app.use('/api', apiRouter);
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname)));
 
--- routes/api.js
+++ routes/api.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 var pokemon_controller = require('../controllers/pokemonController'); 
 
-// GET request to delete Book.
-router.get('/api/pokemon', pokemon_controller.pokemon_list);
+// GET request to show pokemon.
+router.get('/pokemon', pokemon_controller.pokemon_list);
 
 module.exports = router;
