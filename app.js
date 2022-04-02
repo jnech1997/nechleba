172014259384c966026334fac8cb8d8fbdcd669e
Joseph Nechleba <josephnechleba@Josephs-MacBook-Pro.local>
2022-04-02 15:09:30 -0400

    first commit of node app

---
+++ app.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+require("./config/database").connect();
+var helmet = require('helmet');
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+app.use(helmet());
+app.use(express.static(__dirname + '/dist/portfolio'));
+
+app.get('/pokemon', (req, res) => {
+    res.status(200).send('pokemon list returned');
+});
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname)));
+
+module.exports = app;
