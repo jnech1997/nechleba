bd7cfd25fae228b52aeb874ef8b39f9863fae2ac
Joseph Nechleba <josephnechleba@Josephs-MacBook-Pro.local>
2022-04-03 21:59:59 -0400

    force ssl redirect in app

--- app.js
+++ app.js
@@ -5,6 +5,15 @@ var apiRouter = require('./routes/api');
 
 const app = express();
 
+if (process.env.NODE_ENV === 'production') {
+    app.use((req, res, next) => {
+      if (req.header('x-forwarded-proto') !== 'https')
+        res.redirect(`https://${req.header('host')}${req.url}`)
+      else
+        next()
+    })
+}
+
 app.use(express.json());
 app.use(express.static(__dirname + '/dist/portfolio'));
 
