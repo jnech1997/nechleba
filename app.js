365d2ae3127c3261f80d5aecef59bbef479a6f86
Joseph Nechleba <josephnechleba@Josephs-MacBook-Pro.local>
2022-07-11 18:27:55 -0400

    add back redirect, update resume

--- app.js
+++ app.js
@@ -6,14 +6,14 @@ var apiRouter = require('./routes/api');
 
 const app = express();
 
-// if (process.env.NODE_ENV === 'production') {
-//     app.use((req, res, next) => {
-//       if (req.header('x-forwarded-proto') !== 'http')
-//         res.redirect(`http://${req.header('host')}${req.url}`)
-//       else
-//         next()
-//     })
-// }
+if (process.env.NODE_ENV === 'production') {
+    app.use((req, res, next) => {
+      if (req.header('x-forwarded-proto') !== 'https')
+        res.redirect(`https://${req.header('host')}${req.url}`)
+      else
+        next()
+    })
+}
 
 app.use(express.json());
 app.use(express.static(__dirname + '/portfolio/dist/portfolio'));
