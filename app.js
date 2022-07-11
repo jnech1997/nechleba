b1b5eb1b0d74e7d48a412358098dad904f122f11
Joseph Nechleba <josephnechleba@Josephs-MacBook-Pro.local>
2022-07-10 20:43:26 -0400

    remove https to not have to pay for ssl cert

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express();
 if (process.env.NODE_ENV === 'production') {
     app.use((req, res, next) => {
       if (req.header('x-forwarded-proto') !== 'https')
-        res.redirect(`https://${req.header('host')}${req.url}`)
+        res.redirect(`http://${req.header('host')}${req.url}`)
       else
         next()
     })
