612a1b03ef9e6903dd8a5db0c8d44b9372ebd909
Joseph Nechleba <josephnechleba@Josephs-MacBook-Pro.local>
2023-01-24 12:49:54 -0500

    modify data analysis wording

--- portfolio/src/app/sandbox/data-visualizations/data-visualizations.component.ts
+++ portfolio/src/app/sandbox/data-visualizations/data-visualizations.component.ts
@@ -19,7 +19,7 @@ export class DataVisualizationsComponent implements OnInit {
         subtitle: "4/20/2020",
         description: "Taking data from the Global Monitoring Laboratory: https://gml.noaa.gov/ccgg/trends/data.html, we can see that the levels\
         in parts per million of carbon dioxide in the atmosphere has steadily increased in the past several decades. Due to the\
-        Greenhouse Effect, this is likely to create a rise in the temperature of the Earth over time."
+        Greenhouse Effect, this is likely to create a rise in the temperature of the Earth over time as many scientists have already warned."
       }
     },
     {
@@ -31,11 +31,9 @@ export class DataVisualizationsComponent implements OnInit {
         title: "Is the Earth Getting Warmer?",
         subtitle: "5/11/2020",
         description: "This graph shows for a given country, how the maximum average-monthly temperature has changed\
-        over time. You can see that for very high temperature and very low temperature climates, for example Kuwait and Greenland, the max has had a spike\
-        in the last decades. Interesting, the trend is more difficult to see in places of less extreme weather. It is difficult\
-        to say whether climate change affects global temperature patterns uniformly on an average monthly scale. In the\
-        future I'd like to investigate a data set that provides a daily average for countries, which might give more insight into\
-        max temperature anomalies. This data was taken from https://www.kaggle.com/berkeleyearth/climate-change-earth-surface-temperature-data."
+        over time. You can see that for some very high temperature and very low temperature climates, for example Kuwait and Greenland, the max has had a trend of increase\
+        in the last decades. However, as you can also see it is difficult to say whether climate change affects global temperature patterns uniformly on an average-monthly scale. In the\
+        future I'd like to investigate a data set that provides a daily average for countries, which might give more granularity. This data was taken from https://www.kaggle.com/berkeleyearth/climate-change-earth-surface-temperature-data."
       }
     }
   ]
