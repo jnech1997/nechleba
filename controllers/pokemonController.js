efef0ce198c70cebeeca72d526d3e9aae833a10a
jnech1997 <jdn64@cornell.edu>
2025-01-29 20:59:08 -0500

    debug mongoose node error

--- controllers/pokemonController.js
+++ controllers/pokemonController.js
@@ -3,12 +3,17 @@ var Pokemon = require('../models/pokemon');
 // Return list of all Pokemon.
 exports.pokemon_list = function (req, res, next) {
     Pokemon.find()
-        .sort([['name', 'ascending']])
         .then((res) => {
-            console.log("res is: ", res);
             // Successful, so render.
             res.json({list_pokemon});
         }).catch((err) => {
             return next(err);
         });
+        // 
+        // .sort([['name', 'ascending']])
+        // .exec(function (err, list_pokemon) {
+        //     if (err) { return next(err); }
+        //     // Successful, so render.
+        //     res.json({list_pokemon});
+        // })
 };
