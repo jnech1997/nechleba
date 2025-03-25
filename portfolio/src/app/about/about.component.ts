83e8671fdeff94563c80fd51af51dc3f1cd27e50
jnech1997 <jdn64@cornell.edu>
2025-03-25 04:04:03 -0400

    edit about, i18n en translation

--- portfolio/src/app/about/about.component.ts
+++ portfolio/src/app/about/about.component.ts
@@ -9,9 +9,20 @@ import { TranslateService } from '@ngx-translate/core';
 })
 export class AboutComponent implements OnInit {
 
-  constructor(private translate: TranslateService) { }
+  constructor(private translate: TranslateService) {
+    
+  }
 
   ngOnInit() {
+    this.updateClock();
+  }
+
+  updateClock() {
+    let now = new Date().toLocaleString(); // current date
+    // set the content of the element with the ID time to the formatted string
+    document.getElementById('currentTime').innerHTML = now;
+    // call this function again in 1000ms
+    setInterval(this.updateClock, 1000);
   }
 
 }
