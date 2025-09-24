d91f9113cd9c232e49b9e8ce3c6f57d42a8d9a23
Joseph Nechleba <josephnechleba@Josephs-MacBook-Pro.local>
2025-09-24 13:21:35 -0400

    logout user on unauthorized or token expiration

--- portfolio/src/app/interceptors/http.auth-error-interceptor.ts
+++ portfolio/src/app/interceptors/http.auth-error-interceptor.ts
@@ -4,14 +4,16 @@ import { inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { catchError } from 'rxjs/operators';
 import { throwError } from 'rxjs';
+import { AuthService } from '../services/auth.service';
 
 export const AuthErrorInterceptor: HttpInterceptorFn = (req, next) => {
   const router = inject(Router);
+  const auth = inject(AuthService);
   return next(req).pipe(
     catchError((error: HttpErrorResponse) => {
       if (error.status === 401 || error.status === 403) {
         // Clear invalid token
-        localStorage.removeItem('authToken');
+        auth.logout();
         // Redirect to login
         router.navigate(['/projects/sandbox/login'], { queryParams: { returnUrl: router.url } });
       }
